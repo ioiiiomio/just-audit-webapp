6f0b3c3b1636f1fce1c2ada9a57ee3202e6c9c05
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2026-06-27 22:50:55 +0500

    Revert "connected to the neondb"
    This reverts commit 1efd552bdff230e98e8f74d5a3a9892f8ef7a0ab.

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260627_145315 from './20260627_145315';
-
-export const migrations = [
-  {
-    up: migration_20260627_145315.up,
-    down: migration_20260627_145315.down,
-    name: '20260627_145315'
-  },
-];
