5756f12b9c195aa0aeb4620897f3fc2d2ece21e8
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2026-07-11 22:05:42 +0500

    added favicons

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -35,11 +35,25 @@ const montserratAlt = Montserrat_Alternates({
 });
 
 export const metadata: Metadata = {
-  title: "Just Audit",
-  description: "Профессиональные аудиторские услуги в Казахстане",
   icons: {
-    icon: "/icon-tab.svg",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
 };
 
 type Props = {
